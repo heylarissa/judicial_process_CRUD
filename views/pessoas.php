2f635ebbea97ce53e0eb2246733dfa8241cfa8f7
heylarissa <larissah.dandrade@gmail.com>
2023-06-25 02:42:20 -0300

    página de pessoas

---
+++ views/pessoas.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juris - Pessoas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class=container>
+        <h1>Cadastro de pessoas</h1>
+        <br>
+        <table class='table table-dark table-striped table-bordered table-hover'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>CPF/CNPJ</th>
+                    <th>Endereço</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                require '../controllers/processo_controller.php';
+
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
