e7e0f5467a37542e53bf199abb325f703646b260
heylarissa <larissah.dandrade@gmail.com>
2023-06-25 02:42:03 -0300

    user setters

--- models/user.php
+++ models/user.php
@@ -1,5 +1,4 @@
 <?php
-require "pessoa.php";
 
 abstract class User {
     private $id;
@@ -15,33 +14,59 @@ abstract class User {
         return $this->id;
     }
 
+    public function setUserId($id){
+        $this->id = $id;
+    }
+
     public function getFirstName(){
         return $this->first_name;
+    }
+    public function setFirstName($first_name){
+        $this->first_name = $first_name;
     }
 
     public function getLastName(){
         return $this->last_name;
     }
+    public function setLastName($last_name){
+        $this->last_name = $last_name;
+    }
 
     public function getEmail(){
         return $this->email;
     }
+    public function setEmail($email){
+        $this->email = $email;
+    }    
 
     public function getCelular() {
         return $this->celular;
+    }
+    public function setCelular($celular){
+        $this->celular = $celular;
     }
 
     public function getUsername(){
         return $this->username;
     }
+    public function setUsername($username){
+        $this->username = $username;
+    }
 
     public function getPasswd(){
         return $this->passwd;
+    }
+    public function setPasswd($passwd){
+        $this->passwd = $passwd;
     }
 
     public function getPessoa() {
         return $this->pessoa;
     }
+    public function setPessoa(Pessoa $pessoa){
+        $this->pessoa = $pessoa;
+    }
+
 }
 
 ?>
