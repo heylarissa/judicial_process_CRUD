406811825f2d3503bef4f613df1a3fcc969ea246
heylarissa <larissah.dandrade@gmail.com>
2023-06-26 17:32:22 -0300

    tela de login finalizada

--- views/login.php
+++ views/login.php
@@ -1,6 +1,6 @@
 <?php
-  session_start(); // Cria uma sessão ou retoma a sessão atual
-  
+session_start(); // Cria uma sessão ou retoma a sessão atual
+
 ?>
 
 <!DOCTYPE html>
@@ -22,10 +22,10 @@
         include "components/go_back.php";
         ?>
         <h1>Login</h1>
-        <form class="login-form" action="#" method="post">
-            <input label="Username" name="username" required placeholder="Usuário">
-            <input label="Password" name="password" type="Password" required placeholder="Senha" placeholder="sua senha" required>
-            <input type="submit" name="access_btn" value="Login" class="btn-submit">
+        <form class="form-group" style="color:black !important; display:flex; flex-direction: column; align-items: center; gap: 10px;" action="#" method="post">
+            <input class="row" style="color:black !important;" label="Username" name="username" required placeholder="Usuário">
+            <input class="row" style="color:black !important;" label="Password" name="password" type="Password" required placeholder="Senha" placeholder="sua senha" required>
+            <input class="row" style="color:white; background:none;border: 1px solid white; text-align: center;" type="submit" name="access_btn" value="Login" class="btn-submit">
         </form>
         <?php
         require_once "../controllers/user_controller.php";
@@ -35,6 +35,9 @@
             $auth = $controller->authenticate();
         }
         ?>
+        <div class="text-center">
+            <p>Não possui cadastro? <a style="color: white;" href="/register.php">Registre-se</a></p>
+        </div>
     </div>
 </body>
 
